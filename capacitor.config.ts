5180ff753d6f1d61d7d6e6b3096b7de941210f2c
RodrigoFrancisco <rodrigomtfrancisco@gmail.com>
2025-01-14 16:06:38 +0000

    First commit.

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'twins-travells',
+  webDir: 'www'
+};
+
+export default config;
